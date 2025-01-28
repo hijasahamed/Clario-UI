3f234eb38726ed2e23990625152d83604212ec86
hijas <hijasahamed2000@gmail.com>
2025-01-28 12:24:55 +0530

    feat: onboarding screen one developments

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clario/view/screen/onboarding_one.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,14 @@ class ClarioApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
     return MaterialApp(
       title: 'Clario',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: OnboardingOne(screenSize: screenSize,),
     );
   }
 }
---
+++ lib/view/screen/onboarding_one.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnboardingOne extends StatelessWidget {
+  const OnboardingOne({super.key,required this.screenSize});
+  final Size screenSize;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
